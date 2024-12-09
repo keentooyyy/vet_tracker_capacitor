3d2de069c136eee46509aa5bc278beedc084f696
Kentoy <kenneth.lobaton.58@gmail.com>
2024-12-09 20:13:26 +0800

    Added Register Web View

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,3 @@
-
-
 <template>
   <div class="lg:hidden">
 
@@ -12,43 +10,99 @@
     <h1 class="text-2xl text-center my-8 font-bold">Create an Account</h1>
 
 
-
-
     <div class="flex flex-col gap-y-5 w-11/12 mx-auto">
 
-      <input class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" v-model="first_name" placeholder="First Name" type="text"/>
-      <input class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" v-model="last_name" placeholder="Last Name" type="text"/>
-      <input class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" v-model="email" placeholder="Email" type="email"/>
-      <input class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" v-model="password" placeholder="Password" type="password"/>
-      <input class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" v-model="confirm_password" placeholder="Confirm Password" type="password"/>
-      <input class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" v-model="street" placeholder="Street" type="text"/>
-      <input class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" v-model="brgy" placeholder="Brgy." type="text"/>
-      <input class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" v-model="city" placeholder="City" type="text"/>
+      <input
+          v-model="first_name"
+          class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" placeholder="First Name" type="text"/>
+      <input
+          v-model="last_name"
+          class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" placeholder="Last Name" type="text"/>
+      <input
+          v-model="email"
+          class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" placeholder="Email" type="email"/>
+      <input
+          v-model="password"
+          class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" placeholder="Password" type="password"/>
+      <input
+          v-model="confirm_password"
+          class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" placeholder="Confirm Password" type="password"/>
+      <input
+          v-model="street"
+          class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" placeholder="Street" type="text"/>
+      <input
+          v-model="brgy"
+          class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" placeholder="Brgy." type="text"/>
+      <input
+          v-model="city"
+          class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-xl" placeholder="City" type="text"/>
 
       <div class="flex flex-col mb-24">
-        <button @click="registerUser" class="bg-[var(--main-color)] py-5 rounded-md text-white text-xl">Create</button>
-        <router-link class="text-center text-[var(--main-color)] underline text-xl mt-3" to="/">Already have an account?</router-link>
+        <button class="bg-[var(--main-color)] py-5 rounded-md text-white text-xl" @click="registerUser">Create</button>
+        <router-link class="text-center text-[var(--main-color)] underline text-xl mt-3" to="/">Already have an
+          account?
+        </router-link>
       </div>
 
     </div>
 
+  </div>
 
-
-
-
+  <div class="hidden lg:flex">
+    <div class="flex justify-center w-3/5 items-center h-svh">
+      <img alt="Vet Logo" class="w-2/4" src="/images/Logo.png">
+    </div>
+    <div class="bg-white flex flex-col w-2/5 items-center justify-center">
+      <h1 class="text-2xl text-center font-bold mt-10 ">Create an Account</h1>
+      <div class="flex flex-col gap-y-5 w-4/5 mx-auto mt-10 mb-10">
+        <input
+            v-model="first_name"
+            class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-md" placeholder="First Name" type="text"/>
+        <input
+            v-model="last_name"
+            class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-md" placeholder="Last Name" type="text"/>
+        <input
+            v-model="email"
+            class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-md" placeholder="Email" type="email"/>
+        <input
+            v-model="password"
+            class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-md" placeholder="Password" type="password"/>
+        <input
+            v-model="confirm_password"
+            class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-md" placeholder="Confirm Password" type="password"/>
+        <input
+            v-model="street"
+            class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-md" placeholder="Street" type="text"/>
+        <input
+            v-model="brgy"
+            class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-md" placeholder="Brgy." type="text"/>
+        <input
+            v-model="city"
+            class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-md" placeholder="City" type="text"/>
+        <button class="bg-[var(--main-color)] py-5 rounded-md text-white text-xl cursor-pointer mt-3"
+                @click="registerUser">
+          Create
+        </button>
+        <router-link class="text-center text-[var(--main-color)] underline text-xl cursor-pointer" to="/">
+          Already have an
+          account?
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Register',
-  setup(){
+  setup() {
 
 
   },
-  data(){
+  data() {
     return {
       first_name: null,
       last_name: null,
@@ -61,11 +115,11 @@ export default {
       error: null
     }
   },
-  onUpdate(){
+  onUpdate() {
 
   },
   methods: {
-    async registerData(){
+    async registerData() {
       const url = process.env.VUE_APP_API_URL;
       // console.log(url);
       try {
@@ -81,8 +135,7 @@ export default {
           city: this.city,
 
 
-
-        },{
+        }, {
           withCredentials: true
         })
         // console.log(response.data.token);
@@ -96,12 +149,12 @@ export default {
           // Handle case if the response does not contain expected data
           throw new Error('Invalid response data');
         }
-      } catch (err){
+      } catch (err) {
         this.error = err;
         console.log('API request Failed', err)
       }
     },
-    registerUser(){
+    registerUser() {
       this.registerData()
     }
   }

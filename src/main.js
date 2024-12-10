64ba69d52eaf08e76ab187a4a3a4a0b6bca60c08
Kentoy <kenneth.lobaton.58@gmail.com>
2024-12-10 12:10:37 +0800

    Start of Pinia

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 import '/src/assets/tailwind.css';
+import pinia from './store';
 
 
 const app = createApp(App)
+app.use(pinia);
 app.use(router)
 
 

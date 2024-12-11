3c8d74794cefc5eae4ae7c4cd1509d15a174dc25
Kentoy <kenneth.lobaton.58@gmail.com>
2024-12-11 09:54:02 +0800

    appointment fix with css

--- src/views/PetMedicalRecordsView.vue
+++ src/views/PetMedicalRecordsView.vue
@@ -43,6 +43,44 @@
     </button>
   </div>
 
+
+
+
+
+  <teleport to="body">
+    <div v-if="isOpen"
+         class="bg-gray-500 bg-opacity-60 absolute top-0 h-screen w-full flex flex-col justify-center items-center">
+      <div class="w-10/12 md:max-w-screen-sm">
+        <div class="bg-gray-300 rounded-tl-md rounded-tr-md p-3">
+          <div class="cursor-pointer text-[var(--main-color)] text-end" @click="closeModal">X</div>
+        </div>
+        <div class="bg-white py-5 px-3">
+          <select
+              v-model="selectedOption"
+              class="p-4 rounded-md outline outline-2 outline-[var(--secondary-color)] focus:outline-[var(--main-color)] text-md w-3/6"
+          >
+            <option
+                v-for="vaccine_types in vaccine_types_array"
+                :key="vaccine_types.id"
+                :value="vaccine_types.name"
+            >
+              {{ pet_type.name }}
+            </option>
+          </select>
+        </div>
+        <div class="bg-gray-300 rounded-bl-md rounded-br-md p-3 flex gap-x-5">
+          <button
+              class="text-sm outline outline-2 outline-[var(--main-color)] text-[var(--main-color)] rounded-full py-2 px-3 w-3/6"
+              @click="handleSubmit">Submit
+          </button>
+          <button class="text-sm text-white bg-[var(--main-color)] rounded-full py-2 px-3 w-3/6" @click="closeModal">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  </teleport>
+
 </template>
 
 
@@ -57,11 +95,14 @@ export default {
       pet_types_array: [],
       vaccine_types_array: [],
       medical_records_array: [],
+      selectedOption: '',
 
 
       pet_name: '',
       pet_breed: '',
       pet_type: '',
+
+      isOpen: false,
 
       isVet: false,
 

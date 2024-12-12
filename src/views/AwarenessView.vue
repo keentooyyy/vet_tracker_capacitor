04ce084c1182a2226c0051af80a071e1e6080a23
Kentoy <kenneth.lobaton.58@gmail.com>
2024-12-12 23:55:14 +0800

    awareness format fix

--- src/views/AwarenessView.vue
+++ src/views/AwarenessView.vue
@@ -24,35 +24,33 @@ export default {
   <h1 style="padding-left: 5pt;text-indent: 0pt;text-align: left;">RESPONSIBILITIES OF PET OWNERS</h1>
   <p style="text-indent: 0pt;text-align: left;"><br/></p>
   <ol id="l2">
-    <li data-list-text="1."><p style="padding-left: 5pt;text-indent: 0pt;line-height: 107%;text-align: left;">Have your
-      dog regularly vaccinated against rabies starting at 3 months of age and every year therafter.</p></li>
+    <li data-list-text="1."><p style="padding-left: 16pt;text-indent: -11pt;line-height: 107%;text-align: left;">Have
+      your dog regularly vaccinated against rabies starting at 3 months of age and every year therafter.</p></li>
     <li data-list-text="2."><p
-        style="padding-top: 8pt;padding-left: 5pt;text-indent: 0pt;line-height: 112%;text-align: left;">Maintain a
-      registration card which shall contain all vaccinations conducted for accurate record purposes.</p></li>
+        style="padding-top: 8pt;padding-left: 16pt;text-indent: -11pt;line-height: 112%;text-align: left;">Maintain
+      a registration card which shall contain all vaccinations conducted for accurate record purposes.</p></li>
     <li data-list-text="3."><p style="padding-top: 6pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">Submit
       your dog for mandatory registration.</p></li>
     <li data-list-text="4."><p
-        style="padding-top: 9pt;padding-left: 5pt;text-indent: 0pt;line-height: 107%;text-align: left;">Maintain
-      control over your dog and do not allow it to roam the streets or any public places without a</p>
-      <p style="padding-top: 8pt;padding-left: 5pt;text-indent: 0pt;text-align: left;">leash.</p></li>
+        style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;line-height: 107%;text-align: left;">Maintain
+      control over your dog and do not allow it to roam the streets or any public places without a leash.</p></li>
     <li data-list-text="5."><p
-        style="padding-top: 9pt;padding-left: 5pt;text-indent: 0pt;line-height: 107%;text-align: left;">Be a
+        style="padding-top: 8pt;padding-left: 16pt;text-indent: -11pt;line-height: 112%;text-align: left;">Be a
       responsible owner by providing your dog with proper grooming, adequate food and clean shelter.</p></li>
     <li data-list-text="6."><p
-        style="padding-top: 8pt;padding-left: 5pt;text-indent: 0pt;line-height: 110%;text-align: left;">Within 24
+        style="padding-top: 6pt;padding-left: 16pt;text-indent: -11pt;line-height: 110%;text-align: left;">Within 24
       hours, report Immediately any dog biting Incident to the concerned officials for Investigation or for any
       appropriate action and place such dog under observation by a government or private veterinarian.</p></li>
   </ol>
   <p style="text-indent: 0pt;text-align: left;"><br/></p>
   <h1 style="padding-left: 5pt;text-indent: 0pt;text-align: left;">RABIES IN ANIMALS</h1>
-  <p style="padding-top: 9pt;padding-left: 5pt;text-indent: 0pt;line-height: 107%;text-align: justify;">Rabies is a
-    dangerous disease of animals transmissible to humans through bites of infected animals.</p>
-  <p style="padding-top: 8pt;padding-left: 5pt;text-indent: 0pt;line-height: 112%;text-align: left;">Rabies is
-    transmitted
+  <p style="padding-top: 8pt;padding-left: 5pt;text-indent: 0pt;line-height: 112%;text-align: left;">Rabies is a dangerous
+    disease of animals transmissible to humans through bites of infected animals.</p>
+  <p style="padding-top: 6pt;padding-left: 5pt;text-indent: 0pt;line-height: 112%;text-align: left;">Rabies is transmitted
     to other animals through contact with virus- laden saliva by biting or licking.</p>
   <p style="text-indent: 0pt;text-align: left;"><br/></p>
   <h1 style="padding-left: 5pt;text-indent: 0pt;text-align: left;">SIGNS OF CANINE RABIES</h1>
-  <p style="padding-top: 9pt;padding-left: 5pt;text-indent: 0pt;text-align: left;">There are two forms of rabies;</p>
+  <p style="padding-top: 9pt;padding-left: 5pt;text-indent: 0pt;text-align: justify;">There are two forms of rabies;</p>
   <p style="text-indent: 0pt;text-align: left;"><br/></p>
   <ol id="l3">
     <li data-list-text="A."><h1 style="padding-left: 19pt;text-indent: -14pt;text-align: left;">FURIOUS TYPE</h1>
@@ -61,26 +59,26 @@ export default {
           Change from friendly disposition into wild vicious behavior</p></li>
         <li data-list-text="2."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
           Whining as if in pain</p></li>
-        <li data-list-text="3."><p style="padding-top: 8pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
+        <li data-list-text="3."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
           Foaming of the mouth</p></li>
-        <li data-list-text="4."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
+        <li data-list-text="4."><p style="padding-top: 8pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
           If on a leash, bites object within its reach, if caged, bites even the cage</p></li>
         <li data-list-text="5."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
           Difficulty is eating or drinking</p></li>
         <li data-list-text="6."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
           Restlessness</p></li>
-        <li data-list-text="7."><p style="padding-top: 4pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
+        <li data-list-text="7."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
           Runs aimlessly, bites anyone or anything on its path</p></li>
-        <li data-list-text="8."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
+        <li data-list-text="8."><p style="padding-top: 4pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
           Snaps at imaginary objects</p>
           <p style="text-indent: 0pt;text-align: left;"><br/></p></li>
       </ol>
     </li>
     <li data-list-text="B."><h1 style="padding-left: 19pt;text-indent: -14pt;text-align: left;">DUMB TYPE</h1>
       <ol id="l5">
-        <li data-list-text="1."><p style="padding-top: 8pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
+        <li data-list-text="1."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
           Becomes lethargic and depressed. Hides in dark quite places, sluggish or sleepy</p></li>
-        <li data-list-text="2."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
+        <li data-list-text="2."><p style="padding-top: 8pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
           Refused to eat</p></li>
         <li data-list-text="3."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">
           Appears to be starring at a distant object</p></li>
@@ -98,15 +96,15 @@ export default {
   <p style="text-indent: 0pt;text-align: left;"><br/></p>
   <h1 style="padding-left: 5pt;text-indent: 0pt;text-align: left;">MANAGEMENT OF THE BITING DOG</h1>
   <ol id="l6">
-    <li data-list-text="1."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">Restrain
+    <li data-list-text="1."><p style="padding-top: 8pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">Restrain
       the dog with a leash or confine in a cage.</p></li>
     <li data-list-text="2."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">Observe
       the dog or submit the dog to a licensed veterinarian for observation</p></li>
     <li data-list-text="3."><p style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;text-align: left;">Do not
       kill the dog to die naturally</p></li>
     <li data-list-text="4."><p
-        style="padding-top: 9pt;padding-left: 5pt;text-indent: 0pt;line-height: 107%;text-align: left;">If the dog
-      dies within 14 days, call a veterinarian for proper submission of sample to the nearest Diagnostic
+        style="padding-top: 9pt;padding-left: 16pt;text-indent: -11pt;line-height: 107%;text-align: left;">If the
+      dog dies within 14 days, call a veterinarian for proper submission of sample to the nearest Diagnostic
       Laboratory.</p></li>
   </ol>
   <p style="text-indent: 0pt;text-align: left;"><br/></p>
